55470642faae81729a3bb5c79ed02aaadd1b0500
Mine77 <haichaozhu77@gmail.com>
2025-09-19 21:08:09 +0800

    remove data grid

--- artifacts/src/App.tsx
+++ artifacts/src/App.tsx
@@ -1,5 +1,4 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
-import DataGridPage from "./data-grid/DataGridPage";
 import ExcalidrawPage from "./excalidraw/ExcalidrawPage";
 import HomePage from "./HomePage";
 import NoteEditorPage from "./note-editor/NoteEditorPage";
@@ -11,7 +10,6 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/note-editor" element={<NoteEditorPage />} />
-          <Route path="/data-grid" element={<DataGridPage />} />
           <Route path="/excalidraw" element={<ExcalidrawPage />} />
         </Routes>
       </div>
